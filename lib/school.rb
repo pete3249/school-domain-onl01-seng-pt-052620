c551845ab271462d701cb5d6a99f6d76ae5e05e4
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-11 19:26:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+      @roster[grade] << name
+      
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+    end 
+  end 
+ 
+end 
